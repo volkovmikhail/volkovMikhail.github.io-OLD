76dc4f4f8adfa742e36527bb833c2b2eb419dfeb
Mikhail <volkov.work1@gmail.com>
2021-05-19 21:15:29 +0300

    add diod bulb info

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 //parallax
-let bg = document.querySelectorAll('.parallax');
+let bg = document.querySelectorAll(".parallax");
 
-window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-    bg.forEach(element => {
-      element.style.transform = 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)';
-    });  
+window.addEventListener("mousemove", function (e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+  bg.forEach((element) => {
+    element.style.transform =
+      "translate(-" + x * 100 + "px, -" + y * 100 + "px)";
+  });
 });

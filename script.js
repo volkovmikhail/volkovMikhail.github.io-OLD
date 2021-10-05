e7af388fa2c423a139124a6ce210b6f949675c82
Mikhail Volkov <volkov.work1@gmail.com>
2021-10-06 00:32:27 +0300

    test1

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const content = document.querySelector('#content');
-
-async function data(){
-   const data = await (await fetch('https://chatik-node.herokuapp.com/messages')).json()
-
-   content.innerHTML = JSON.stringify(data);
-}
-
-data()
+this.location.href = 'https://chatik-node.herokuapp.com'

3a4759509dc099101cff76b4947177905e260c9e
volkovMikhail <volkov.work1@gmail.com>
2022-03-19 12:20:53 +0300

    change styles for better view

--- scripts/memes.js
+++ scripts/memes.js
@@ -1,16 +1,13 @@
 const API_URL = 'https://meme-api.herokuapp.com/gimme/';
 const memesContainer = document.getElementsByTagName('main')[0];
-const pixelsToAddMemes = 5000;
+const pixelsToAddMemes = 4000;
 console.log('Memes engaged');
 
 async function fetchMemes() {
   console.log('rendered');
   const memes = await (await fetch(`${API_URL}40`)).json();
   memes.memes.forEach((mem) => {
-    memesContainer.insertAdjacentHTML(
-      'beforeend',
-      `<div class="image" style="background-image: url('${mem.url}'); margin-bottom: 1rem;">`
-    );
+    memesContainer.insertAdjacentHTML('beforeend', `<img src="${mem.url}">`);
   });
 }
 
